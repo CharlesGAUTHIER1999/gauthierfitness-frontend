81330afef7f58283499176f302a6e3c84f2bd30c
CharlesGAUTHIER1999 <charles.gauthier99@gmail.com>
2026-07-07 18:41:37 +0200

    Resolve conflicts (V1GF-Ready)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -33,7 +33,7 @@ export default function Header() {
         <header className="header">
             <div className="header-inner">
                 <Link to="/" className="logo" onClick={closeMenu}>
-                    <img className="logo-img" src="/logos/logo-gf.png" alt="GauthierFitness" width="55" height="32"/>
+                    <img className="logo-img" src="/logos/logo-gf.png" alt="GauthierFitness" width="89" height="32"/>
                     <span>GAUTHIER Fitness</span>
                 </Link>
 
