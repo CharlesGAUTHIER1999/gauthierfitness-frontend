ca6169bc342dbe16d07ce1ea8f0dd7985153aa83
CharlesGAUTHIER1999 <charles.gauthier99@gmail.com>
2026-07-10 00:41:47 +0200

    GF32-V1GF-Finished

--- src/pages/static/Help.jsx
+++ src/pages/static/Help.jsx
@@ -1,22 +1,26 @@
 import {Link} from "react-router-dom";
 import StaticPage from "../../components/StaticPage.jsx";
+import Footer from "../../components/Footer.jsx";
 
 // Static "Help center" placeholder page (feature coming soon)
 export default function Help() {
     return (
-        <StaticPage title="Centre d'aide">
-            <div className="coming-soon">
-                <span className="coming-soon__badge">En cours de réalisation</span>
-                <h2>Notre centre d'aide arrive bientôt</h2>
-                <p>
-                    Nous préparons une FAQ complète (commandes, personnalisation,
-                    paiement, suivi…). En attendant, notre équipe répond
-                    directement à tes questions.
-                </p>
-                <Link to="/contact" className="btn">
-                    Nous contacter
-                </Link>
-            </div>
-        </StaticPage>
+        <div>
+            <StaticPage title="Centre d'aide">
+                <div className="coming-soon">
+                    <span className="coming-soon__badge">En cours de réalisation</span>
+                    <h2>Notre centre d'aide arrive bientôt</h2>
+                    <p>
+                        Nous préparons une FAQ complète (commandes, personnalisation,
+                        paiement, suivi…). En attendant, notre équipe répond
+                        directement à tes questions.
+                    </p>
+                    <Link to="/contact" className="btn">
+                        Nous contacter
+                    </Link>
+                </div>
+            </StaticPage>
+            <Footer/>
+        </div>
     );
 }
